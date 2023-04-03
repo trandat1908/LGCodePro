f78fe7bdc9265dee125d79fe07d2f98d5a1c24b7
trandat1908 <44970952+trandat1908@users.noreply.github.com>
2023-04-03 23:06:07 +0700

    Update WordSearch.cpp

--- WordSearch.cpp
+++ WordSearch.cpp
@@ -33,54 +33,62 @@ Additional practice:
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<pair<int,int>> pos;
+    vector<pair<int, int>> pos;
     int visited[10][10] = {0};
     int dx[4] = {0, 1, 0, -1};
     int dy[4] = {1, 0, -1, 0};
     int n, m, l;
-    bool dfs(vector<vector<char>>& board,const string& word, pair<int, int> p, string s) {
-        if((int)s.length() == l) {
+    bool dfs(vector<vector<char>> &board, const string &word, pair<int, int> p, string s)
+    {
+        if ((int)s.length() == l)
+        {
             return true;
         }
-        for(int i=0; i<4; ++i) {
+        for (int i = 0; i < 4; ++i)
+        {
             int tx = p.first + dx[i];
             int ty = p.second + dy[i];
-            cout<<"tx= "<<tx<<" ty= "<<ty<<endl;
-            cout<<"s= "<<s<<endl;
-            if(tx>=0&&tx<n&&ty>=0&&ty<m&&visited[tx][ty]==0) {
-                if(board[tx][ty]==word[s.length()]) {
+            if (tx >= 0 && tx < n && ty >= 0 && ty < m && visited[tx][ty] == 0)
+            {
+                if (board[tx][ty] == word[s.length()])
+                {
                     visited[tx][ty] = 1;
                     s.push_back(board[tx][ty]);
-                    pair<int,int> pp = {tx, ty};
-                    return dfs(board, word, pp, s);
+                    pair<int, int> pp = {tx, ty};
+                    if (dfs(board, word, pp, s))
+                        return true;
                     s.pop_back();
                     visited[tx][ty] = 0;
                 }
-            }
+                
+            } 
         }
         return false;
     }
-    bool exist(vector<vector<char>>& board, string word) {
-
+    bool exist(vector<vector<char>> &board, string word)
+    {
         n = board.size();
         m = board[0].size();
         l = word.length();
-        for(int i=0; i<n; ++i) {
-            for(int j=0; j<m; ++j) {
-                if(board[i][j]==word[0]) {
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < m; ++j)
+            {
+                if (board[i][j] == word[0])
+                {
                     pos.push_back(make_pair(i, j));
                 }
             }
         }
-
-
-        for(int i=0; i<(int) pos.size(); ++i) {
+        for (int i = 0; i < (int)pos.size(); ++i)
+        {
             string s = "";
             visited[pos[i].first][pos[i].second] = 1;
             s.push_back(word[0]);
-            if(dfs(board, word, pos[i], s))
+            if (dfs(board, word, pos[i], s))
                 return true;
             visited[pos[i].first][pos[i].second] = 0;
         }
@@ -88,9 +96,10 @@ public:
     }
 };
 
-int main() {
+int main()
+{
     Solution *s = new Solution;
-    vector<vector<char>> board = {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
+    vector<vector<char>> board = {{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
     string word = "ABCEFSADEESE";
     cout << s->exist(board, word) << endl;
     return 0;
