f1209d5845e166e8be11a1270ee40cd6e80bdb30
trandat1908 <44970952+trandat1908@users.noreply.github.com>
2023-04-19 22:38:28 +0700

    Update MinimumCostToHireKWorkers.cpp

--- MinimumCostToHireKWorkers.cpp
+++ MinimumCostToHireKWorkers.cpp
@@ -25,3 +25,38 @@ public:
         return ans;
     }
 };
+
+
+bool internalCompare(pair<int, int>& p1, pair<int,int>& p2) {
+    return (double) p1.second/p1.first < (double) p2.second/p2.first;
+}
+
+class Solution {
+public:
+    double mincostToHireWorkers(vector<int>& quality, vector<int>& wage, int K) {
+        int N = quality.size();
+        vector<pair<int,int>> workers(N);
+        for (int i = 0; i < N; ++i) {
+            workers[i].first = quality[i];
+            workers[i].second = wage[i];
+        }
+        
+        sort(workers.begin(), workers.end(), internalCompare);
+
+        double ans = 1e9;
+        int sumq = 0;
+        priority_queue<int> pool;
+        for (auto worker : workers) {
+            pool.push(worker.first);
+            sumq += worker.first;
+            if (pool.size() > K) {
+                sumq -= pool.top(); pool.pop();
+            }
+                
+            if (pool.size() == K) {
+                ans = min(ans, sumq * ((double)worker.second/worker.first));
+            }
+        }
+        return ans;
+    }
+};
